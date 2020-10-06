f583c83ffd35b44a1fe99669ebb815a2a5738ce6
misrar <rohitmisra.44@gmail.com>
2020-10-06 23:54:44 +0200

    config file

---
+++ server/src/config.ts
@@ -0,0 +1,13 @@
+export const GOOGLE = {
+  CLIENT_ID: "<google-client-id>",
+  CLIENT_SECRET: "<google-client-secret>",
+  AUTH_URL: "https://accounts.google.com/o/oauth2/v2/auth",
+  TOKEN_URL: "https://www.googleapis.com/oauth2/v4/token"
+};
+
+export const KEY_CLOAK = {
+  CLIENT_ID: "<keycloak-client-id>",
+  CLIENT_SECRET: "<keycloak-client-secret>",
+  AUTH_URL: "http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/auth",
+  TOKEN_URL: "http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/token"
+};
